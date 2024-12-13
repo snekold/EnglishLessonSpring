b1eaeb7f4c91e572ac10c0f99b32b80d9fd5a924
Snekold <e89030066143@yandex.ru>
2024-12-13 21:08:46 +0300

    add security

---
+++ src/main/java/org/example/englishlessonspring/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package org.example.englishlessonspring.security;
+
+import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+
+//@Configuration
+//@EnableWebSecurity
+
+public class SecurityConfig {
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests(auth-> auth
+//                .requestMatchers("/lk").authenticated()
+//                .anyRequest().permitAll()
+//        );
+//        return http.build();
+//    }
+
+}
---
+++ src/main/java/org/example/englishlessonspring/security/SimpleUserDetails.java
@@ -0,0 +1,26 @@
+package org.example.englishlessonspring.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class SimpleUserDetails implements UserDetails {
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
